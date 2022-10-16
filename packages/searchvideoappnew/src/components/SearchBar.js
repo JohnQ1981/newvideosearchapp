6f4905e583143ccb7d6ed52c1129858a80d64f4b
JohnQ1981 <ikram1981@gmail.com>
2022-10-15 21:26:27 -0500

    from class based component to functional component

--- packages/searchvideoappnew/src/components/SearchBar.js
+++ packages/searchvideoappnew/src/components/SearchBar.js
@@ -1,35 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+const SearchBar = ({onFormSubmit}) => {
+  const [term, setTerm] = useState("");
 
-class SearchBar extends React.Component {
-  state = { term: "" };
-
-  onInputChange = event => {
-    this.setState({ term: event.target.value });
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
   };
-  onFormSubmit = event => {
+  const onSubmit = (event) => {
     event.preventDefault();
     //TODO: Make sure we call call back from parent component
-    this.props.onFormSubmit(this.state.term);
+    onFormSubmit(term);
   };
-
-  render() {
-    return (
-      <div className="search-bar ui segment">
+  return (
+    <div className="search-bar ui segment">
       <p>This App is built with React!</p>
-        <hr></hr>
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+      <hr></hr>
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={onInputChange}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+// class SearchBar extends React.Component {
+//   //state = { term: "" };
+
+//   // onInputChange = event => {
+//   //   this.setState({ term: event.target.value });
+//   // };
+//   // onFormSubmit = event => {
+//   //   event.preventDefault();
+//   //   //TODO: Make sure we call call back from parent component
+//   //   this.props.onFormSubmit(this.state.term);
+//   // };
+
+//   render() {
+//     // return (
+//     //   <div className="search-bar ui segment">
+//     //     <p>This App is built with React!</p>
+//     //     <hr></hr>
+//     //     <form onSubmit={this.onFormSubmit} className="ui form">
+//     //       <div className="field">
+//     //         <label>Video Search</label>
+//     //         <input
+//     //           type="text"
+//     //           value={this.state.term}
+//     //           onChange={this.onInputChange}
+//     //         />
+//     //       </div>
+//     //     </form>
+//     //   </div>
+//     // );
+//   }
+// }
 
 export default SearchBar;
