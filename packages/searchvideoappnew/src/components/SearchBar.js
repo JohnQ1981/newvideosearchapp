6e874fa1f0d5e5718bed54d1d6c9a7ccbd08996d
JohnQ1981 <ikram1981@gmail.com>
2022-10-16 20:55:14 -0500

    connected vercel

--- packages/searchvideoappnew/src/components/SearchBar.js
+++ packages/searchvideoappnew/src/components/SearchBar.js
@@ -16,7 +16,7 @@ const SearchBar = ({ onFormSubmit }) => {
       <hr></hr>
       <form onSubmit={onSubmit} className="ui form">
         <div className="field">
-          <label>Video Search on YouTube!</label>
+          <label>Search a Video on YouTube!</label>
           <input type="text" value={term} onChange={onInputChange} />
         </div>
       </form>
